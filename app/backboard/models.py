72794bbab795af781c27428bda1a5315d71c4e15
Edison Wang <ew040607@gmail.com>
2026-07-17 22:21:07 -0400

    init: feature slice files

---
+++ app/backboard/models.py
@@ -0,0 +1,1 @@
+from app.hackplate.plates.db_plates.mongo.registry import register_document  # noqa: F401
--- migrations/register_models.py
+++ migrations/register_models.py
@@ -1,1 +1,7 @@
 import migrations.register_auth_model  # noqa: F401
+import app.orgs.models  # noqa: F401
+import app.mcp.models  # noqa: F401
+import app.file_upload.models  # noqa: F401
+import app.claude_hook.models  # noqa: F401
+import app.backboard.models  # noqa: F401
+import app.graph.models  # noqa: F401
